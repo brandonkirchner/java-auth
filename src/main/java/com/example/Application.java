b6268d01bac6d55ae457078fa4a875500854ee29
brandon <brandon.kirchner@blackbaud.com>
2016-01-19 12:20:21 -0600

    wiring up the database

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,6 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +16,14 @@ public class Application {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 	}
+
+    @Configuration
+    public class DatabaseConfig {
+        @Bean
+        @Primary
+        @ConfigurationProperties(prefix = "spring.datasource")
+        public DataSource dataSource() {
+            return DataSourceBuilder.create().build();
+        }
+    }
 }
