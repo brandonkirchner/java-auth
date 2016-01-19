6fe9f02543bacb8ec83fbdc59965aa1f0fc752ab
brandon <brandon.kirchner@blackbaud.com>
2016-01-19 12:35:43 -0600

    add some tests (like a boss)

---
+++ src/test/java/com/example/AuthControllerTests.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class AuthControllerTests {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new AuthController()).build();
+    }
+
+    @Test
+    public void getLogin() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/login").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("You've logged in!")));
+    }
+
+    @Test
+    public void getLogout() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/logout").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("You've logged out!")));
+    }
+}
